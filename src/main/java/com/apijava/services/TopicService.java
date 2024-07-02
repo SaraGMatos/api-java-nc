9bb47b5a4f0f6331ba2a99c0ea9bcb92d50cfa83
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-02 15:36:40 +0100

    Removes unused code

--- src/main/java/com/apijava/services/TopicService.java
+++ src/main/java/com/apijava/services/TopicService.java
@@ -5,7 +5,6 @@ import java.util.Objects;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.apijava.exceptions.NotFoundException;
 import com.apijava.persistence.entities.Topic;
