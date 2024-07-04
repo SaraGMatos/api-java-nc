6e211483cb53f7e484f44f1166abf851b32cab19
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-04 15:41:02 +0100

    Removes unused import

--- src/main/java/com/apijava/persistence/entities/Article.java
+++ src/main/java/com/apijava/persistence/entities/Article.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 
 @Entity
